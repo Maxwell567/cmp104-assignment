24cb365f674d1640bef631bc85c908f3c0340f46
Maxwell567 <57185767+Maxwell567@users.noreply.github.com>
2020-05-03 22:58:33 +0100

    via add

---
+++ cmp104.py
@@ -0,0 +1,124 @@
+WAKAYE MAYAMBA MAXWELL
+BHU\19\04\05\0027
+
+
+#To calculate the area of a square
+print("Calculate the area of a Square (Formula :a x a:)")\n
+a = float(input("What is A: "))
+b = a * a
+print("Equalls to",b)
+
+
+#To calculate the area of a Rectangle
+print("Calculate the area of a Rectangle (Formula :l x b:)")\n
+l = float(input("What is L: "))
+b = float(input("what is b: "))
+r = l * b
+print("Equalls to",r)
+
+
+#To calculate the area of a Triangle
+print("Calculate the area of a Triangle (Formula :1/2 x b x h:)")\n
+b = float(input("what is b: "))
+h = float(input("Enter the Value of h: "))
+r = 1 / 2 * (b * h)
+print("Equalls to",r)
+
+
+#To calculate the area of a Trapezoid
+print("Calculate the area of a Triangle (Formula:1/2 x (b1 + b2)xh:)")\n
+b1 = float(input("what is b1: "))
+b2= float(input("what is b2: "))
+h= float(input("Enter the Value of h: "))
+r = 1 / 2 * (b1 + b2) * h
+print("Equalls to",r)
+
+
+#To calculate the area of a Circle
+print("Calculate the area of a Cicle (Formula :PIxrxr:)")\n
+rd = float(input("Enter the Value of r: "))
+pi = 22/7
+s = rd * rd
+r = pi * s
+print("Equalls to",r)
+
+
+#To calculate Surcface area of a Cube
+print("Calculate Surcface area of a Cube (Formula :2xPIxr:)")\n
+rd = float(input("Enter the Value of r: "))
+pi = 22/7
+r = 2 * pi * rd
+print("Equalls to",r)
+
+
+#To calculate the Surcface area of a Cube
+print("Calculate the Surcface area of a Cube (Formula :6xaxa:)")\n
+a = float(input("What is A: "))
+s = a * a 
+r = 6 * s
+print("Equalls to",r)
+
+
+#To calculate the curved surface area of a cylinder 
+print("Calculate curved surface area of a cylinder (Formula :2PIxrx(r+h):)")\n
+rd = float(input("Enter the Value of r: "))
+h = float(input("Enter the Value of h: "))
+pi = 22/7
+r = 2 * pi * rd * ( rd + h)
+print("Equalls to",r)
+
+
+#To calculate the Volume of a Cyinder
+print("Calculate the Volume of a Cyinder(Formula :PIx(rxr)*h:)")\n
+rd = float(input("Enter the Value of r: "))
+h = float(input("Enter the Value of h: "))
+pi = 22/7
+s = rd * rd
+r = pi * s * h
+print("Equalls to",r)
+
+
+#To calculate the Acceleration Formula
+print("Calculate the  Acceleration Formula(Formula :V-U/t:)")\n
+v = float(input("Enter the Value of v: "))
+u = float(input("Enter the Value of u: "))
+t = float(input("Enter the Value of t: "))
+r = (v - u) / t
+print("Equalls to",r)
+
+
+#To calculate the Density Formula
+print("Calculate the Density Formula(Formula :M/V:)")\n
+m = float(input("Enter the Value of m: "))
+v = float(input("Enter the Value of v: "))
+r = m / v
+print("Equalls to",r)
+
+
+#To calculate the Formula for pressure
+print("Calculate the Volume of a Cyinder(Formula :F/A:)")\n
+f = float(input("Enter the Value of f: "))
+a = float(input("What is A: "))
+r = f / a
+print("Equalls to",r)
+
+
+#To calculate the formula for kinetic Energy
+print("Calculate the formula for kinetic Energy(Formula :1/2M(vxv):)")\n
+m = float(input("Enter the Value of m: "))
+v = float(input("Enter the Value of v: "))
+s = v * v 
+r = 1/2 * m * s
+print("Equalls to",r)
+
+
+#To calculate the formula for voltage
+print("Calculate the formula for voltage(Formula :IR:)")\n
+i = float(input("Enter the Value of i: "))
+r = float(input("Enter the Value of r: "))
+ro = i * r
+print("Equalls to",ro)
+
+WAKAYE MAYAMBA MAXWELL
+
+BHU\19\04\05\0027
